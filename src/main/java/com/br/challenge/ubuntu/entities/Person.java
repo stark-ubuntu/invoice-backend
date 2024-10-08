d89fbafbd582af4074a92f0b96a5ac8a06d508ff
Matheus Fideles <matheuss.fideles@hotmail.com>
2024-10-08 00:32:17 -0300

    create entity Person

---
+++ src/main/java/com/br/challenge/ubuntu/entities/Person.java
@@ -0,0 +1,26 @@
+package com.br.challenge.ubuntu.entities;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "person", schema = "invoice")
+public class Person extends PanacheEntityBase {
+
+    @Id
+    private UUID id;
+    private String name;
+    private String taxId;
+}
